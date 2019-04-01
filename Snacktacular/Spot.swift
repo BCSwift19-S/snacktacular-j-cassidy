aeb3452d30ddc574a610f9fa96eeac55f956274e
Jimmy Cassidy <cassidjf@bc.edu>
2019-04-01 16:17:45 -0400

    9.7 Complete

--- Snacktacular/Spot.swift
+++ Snacktacular/Spot.swift
@@ -9,8 +9,9 @@
 import Foundation
 import CoreLocation
 import Firebase
+import MapKit
 
-class Spot {
+class Spot: NSObject, MKAnnotation {
     var name: String
     var address: String
     var coordinate: CLLocationCoordinate2D
@@ -25,6 +26,14 @@ class Spot {
     
     var latitude: CLLocationDegrees {
         return coordinate.latitude
+    }
+    
+    var title: String? {
+        return name
+    }
+    
+    var subtitle: String? {
+        return address
     }
     
     var dictionary: [String: Any] {
@@ -42,7 +51,7 @@ class Spot {
         
     }
     
-    convenience init() {
+    convenience override init() {
         self.init(name: "", address: "", coordinate: CLLocationCoordinate2D(), averageRating: 0.0, numberOfReviews: 0, postingUserID: "", documentID: "")
     }
     
