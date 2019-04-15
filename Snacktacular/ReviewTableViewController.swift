6a2bfc81e18498d6c185fb0754746164e9e0bd80
Jimmy Cassidy <cassidjf@bc.edu>
2019-04-15 15:58:55 -0400

    9.13 Complete

--- Snacktacular/ReviewTableViewController.swift
+++ Snacktacular/ReviewTableViewController.swift
@@ -20,6 +20,17 @@ class ReviewTableViewController: UITableViewController {
     @IBOutlet weak var deleteButton: UIButton!
     @IBOutlet weak var buttonsBackgroundView: UIView!
     @IBOutlet weak var reviewDateLabel: UILabel!
+    @IBOutlet var starButtonCollection: [UIButton]!
+    
+    var rating = 0 {
+        didSet {
+            for starButton in starButtonCollection {
+                let image = UIImage(named: ( starButton.tag < rating ? "star-filled" : "star-empty"))
+                starButton.setImage(image, for: .normal)
+            }
+            print("*** new rating \(rating)")
+        }
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -36,6 +47,11 @@ class ReviewTableViewController: UITableViewController {
         } else {
             navigationController?.popViewController(animated: true)
         }
+    }
+    
+    @IBAction func starButtonPressed(_ sender: UIButton) {
+        rating = sender.tag + 1
+        
     }
     
     @IBAction func saveButtonPressed(_ sender: UIBarButtonItem) {
