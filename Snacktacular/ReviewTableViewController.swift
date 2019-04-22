d63b164120b6c5173ee86e53b73605cf8855b344
Jimmy Cassidy <cassidjf@bc.edu>
2019-04-22 15:37:37 -0400

    9.22 Complete

--- Snacktacular/ReviewTableViewController.swift
+++ Snacktacular/ReviewTableViewController.swift
@@ -140,6 +140,13 @@ class ReviewTableViewController: UITableViewController {
     }
     
     @IBAction func deleteButtonPressed(_ sender: UIButton) {
+        review.deleteData(spot: spot) { (success) in
+            if success {
+                self.leaveViewController()
+            } else {
+                print("*** error: delete unsuccessful")
+            }
+        }
     }
     
     @IBAction func reviewTitleChanged(_ sender: UITextField) {
